a0b63f07b86317fb306b862dde1d3d74c413fedc
Martin Jambon <martin@mjambon.com>
2020-05-06 19:33:35 -0700

    Add comments that were lost in translation.

--- lib/ast_grammar.ml
+++ lib/ast_grammar.ml
@@ -1,12 +1,21 @@
-type ident = string[@@deriving show]
+(* Essence of grammar.json content *)
+
+type ident = string [@@deriving show]
+
 type rule_body =
-  | REPEAT of rule_body 
-  | CHOICE of rule_body list 
-  | SEQ of rule_body list 
-  | SYMBOL of ident 
-  | STRING of string 
+  (* composite (nodes) *)
+  | REPEAT of rule_body
+  | CHOICE of rule_body list
+  | SEQ of rule_body list
+
+  (* atomic (leaves) *)
+  | SYMBOL of ident
+  | STRING of string
   | PATTERN of string [@@deriving show { with_path = false }]
-type rule = (ident * rule_body)[@@deriving show]
-type rules = rule list[@@deriving show]
-type grammar = (ident * rules)[@@deriving show]
-type t = grammar[@@deriving show]
+
+type rule = (ident * rule_body) [@@deriving show]
+type rules = rule list [@@deriving show]
+type grammar = (ident * rules) [@@deriving show]
+
+(* alias *)
+type t = grammar [@@deriving show]
--- lib/parse_grammar.ml
+++ lib/parse_grammar.ml
@@ -1,69 +1,84 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2020 r2c
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * file license.txt for more details.
+ *)
+
 open Common
 open Ast_grammar
 module J = Json_type
+
 let error s json =
   failwith (spf "Wrong format: %s, got: %s" s (Json_io.string_of_json json))
+
 let rec parse_body json =
   match json with
   | ((J.Object
-      (("type", ((J.String ("REPEAT"))[@explicit_arity ]))::("content", json)::[]))
-      [@explicit_arity ]) -> ((REPEAT ((parse_body json)))[@explicit_arity ])
+        (("type", ((J.String ("REPEAT"))))::("content", json)::[]))
+    ) -> ((REPEAT ((parse_body json))))
   | ((J.Object
-      (("type", ((J.String ("CHOICE"))[@explicit_arity ]))::("members",
-                                                             ((J.Array
-                                                             (xs))[@explicit_arity
-                                                                    ]))::[]))
-      [@explicit_arity ]) -> ((CHOICE ((List.map parse_body xs)))
-      [@explicit_arity ])
+        (("type", ((J.String ("CHOICE"))))::("members",
+                                             ((J.Array
+                                                 (xs))))::[]))
+    ) -> ((CHOICE ((List.map parse_body xs)))
+         )
   | ((J.Object
-      (("type", ((J.String ("SEQ"))[@explicit_arity ]))::("members",
-                                                          ((J.Array
-                                                          (xs))[@explicit_arity
-                                                                 ]))::[]))
-      [@explicit_arity ]) -> ((SEQ ((List.map parse_body xs)))
-      [@explicit_arity ])
+        (("type", ((J.String ("SEQ"))))::("members",
+                                          ((J.Array
+                                              (xs))))::[]))
+    ) -> ((SEQ ((List.map parse_body xs)))
+         )
   | ((J.Object
-      (("type", ((J.String ("SYMBOL"))[@explicit_arity ]))::("name",
-                                                             ((J.String
-                                                             (name))[@explicit_arity
-                                                                    ]))::[]))
-      [@explicit_arity ]) -> ((SYMBOL (name))[@explicit_arity ])
+        (("type", ((J.String ("SYMBOL"))))::("name",
+                                             ((J.String
+                                                 (name))))::[]))
+    ) -> ((SYMBOL (name)))
   | ((J.Object
-      (("type", ((J.String ("STRING"))[@explicit_arity ]))::("value",
-                                                             ((J.String
-                                                             (name))[@explicit_arity
-                                                                    ]))::[]))
-      [@explicit_arity ]) -> ((STRING (name))[@explicit_arity ])
+        (("type", ((J.String ("STRING"))))::("value",
+                                             ((J.String
+                                                 (name))))::[]))
+    ) -> ((STRING (name)))
   | ((J.Object
-      (("type", ((J.String ("PATTERN"))[@explicit_arity ]))::("value",
-                                                              ((J.String
-                                                              (name))
-                                                              [@explicit_arity
-                                                                ]))::[]))
-      [@explicit_arity ]) -> ((PATTERN (name))[@explicit_arity ])
+        (("type", ((J.String ("PATTERN"))))::("value",
+                                              ((J.String
+                                                  (name))
+                                              ))::[]))
+    ) -> ((PATTERN (name)))
   | ((J.Object
-      (("type", ((J.String
-        (("PREC"|"PREC_LEFT"|"PREC_RIGHT"|"PREC_DYNAMIC")))[@explicit_arity ]))::
-       ("value", _)::("content", json)::[]))[@explicit_arity ])
-      -> parse_body json
+        (("type", ((J.String
+                      (("PREC"|"PREC_LEFT"|"PREC_RIGHT"|"PREC_DYNAMIC")))))::
+         ("value", _)::("content", json)::[])))
+    -> parse_body json
   | ((J.Object
-      (("type", ((J.String ("FIELD"))[@explicit_arity ]))::("name",
-                                                            ((J.String
-                                                            (_name))[@explicit_arity
-                                                                    ]))::
-       ("content", json)::[]))[@explicit_arity ])
-      -> parse_body json
+        (("type", ((J.String ("FIELD"))))::("name",
+                                            ((J.String
+                                                (_name))))::
+         ("content", json)::[])))
+    -> parse_body json
   | _ -> error "parse_body" json
+
 let parse_rule (name, json) = (name, (parse_body json))
+
 let parse_rules xs = List.map parse_rule xs
+
 let parse_grammar json =
   match json with
-  | ((J.Object ((start, x)::rest))[@explicit_arity ]) ->
+  | ((J.Object ((start, x)::rest))) ->
       (start, (parse_rules ((start, x) :: rest)))
   | _ -> error "Grammar" json
+
+(* Entrypoint *)
 let parse file =
   let json = Json_io.load_json file in
   match json with
-  | ((J.Object (xs))[@explicit_arity ]) ->
+  | ((J.Object (xs))) ->
       let rules = List.assoc "rules" xs in parse_grammar rules
   | _ -> error "Top" json
