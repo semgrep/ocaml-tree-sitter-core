6026ece212c52e42a93b4bc66b632439b1b8e8ac
Martin Jambon <martin@mjambon.com>
2020-05-21 00:33:26 -0700

    Fix unit tests

--- src/gen/test/Fresh.ml
+++ src/gen/test/Fresh.ml
@@ -2,24 +2,33 @@
    Unit tests for the Fresh module.
 *)
 
+open Printf
 open Ocaml_tree_sitter_gen
 
 let test_simple () =
   let scope = Fresh.create_scope () in
-  let add name = Fresh.create_name scope name in
+  let add name =
+    let res = Fresh.create_name scope name in
+    printf "%s -> %s\n%!" name res;
+    res
+  in
   assert (add "x" = "x");
   assert (add "y" = "y");
   assert (add "x" = "x_");
-  assert (add "x" = "x1")
+  assert (add "x" = "x2")
 
 let test_prefix_prefix () =
   let scope = Fresh.create_scope () in
-  let add name = Fresh.create_name scope name in
+  let add name =
+    let res = Fresh.create_name scope name in
+    printf "%s -> %s\n%!" name res;
+    res
+  in
   assert (add "x" = "x");
   assert (add "x_" = "x_");
-  assert (add "x" = "x1");
+  assert (add "x" = "x2");
   assert (add "x_" = "x__");
-  assert (add "x_" = "x_1")
+  assert (add "x_" = "x_2")
 
 let test = "Disambiguate", [
   "simple", `Quick, test_simple;
