be6638b3d2693c6c4b32b076ba832b9be3e47bdb
Ruin0x11 <ipickering2@gmail.com>
2021-01-10 23:29:21 -0800

    Extend semgrep-rust with semgrep syntax

--- src/semgrep-rust/grammar.js
+++ src/semgrep-rust/grammar.js
@@ -1,15 +1,60 @@
 /*
  * semgrep-rust
  *
- * Extend the standard rust grammar with metavariable pattern constructs.
- * There is no need to extend it for ellipsis because ellipsis are already
- * part of the Rust language.
+ * Extend the original tree-sitter Rust grammar with semgrep-specific constructs
+ * used to represent semgrep patterns.
  */
 
 const standard_grammar = require('tree-sitter-rust/grammar');
 
 module.exports = grammar(standard_grammar, {
-    name: 'rust',
+  name: 'rust',
 
-    rules: {}
+  rules: {
+
+    // Entry point
+    source_file: ($, previous) => {
+      return choice(
+        previous,
+        $.semgrep_expression
+      );
+    },
+
+    // Alternate "entry point". Allows parsing a standalone expression.
+    semgrep_expression: $ => seq('__SEMGREP_EXPRESSION', $._expression),
+
+    // Metavariables
+    identifier: ($, previous) => {
+      return token(
+        choice(
+          previous,
+          /\$[A-Z_][A-Z_0-9]*/
+        )
+      );
+    },
+
+    // Statement ellipsis: '...' not followed by ';'
+    _expression_statement: ($, previous) => {
+      return choice(
+        previous,
+        prec.right(100, seq($.ellipsis, ';')),  // expression ellipsis
+        prec.right(100, $.ellipsis),  // statement ellipsis
+      );
+    },
+
+    // Expression ellipsis
+    _expression: ($, previous) => {
+      return choice(
+        ...previous.members,
+        $.ellipsis,
+        $.deep_ellipsis
+      );
+    },
+
+    deep_ellipsis: $ => seq(
+      '<...', $._expression, '...>'
+    ),
+
+    ellipsis: $ => '...',
+  }
 });
