00df79920b2b7bda2ccc7162706e8dc5d3e3f40c
Colleen Dai <colleen@returntocorp.com>
2021-01-21 14:00:52 -0800

    added to grammar for metavariables and ellipsis. Also wrote some pretty simple test cases.

--- src/semgrep-kotlin/grammar.js
+++ src/semgrep-kotlin/grammar.js
@@ -11,6 +11,34 @@ module.exports = grammar(standard_grammar, {
     name: 'kotlin',
 
     rules: {
-        identifier: $ => /\$?[a-zA-Z_][a-zA-Z_0-9]*/
+
+        // Entry point
+        source_file: ($, previous) => {
+          return choice(
+            previous,
+            $.semgrep_expression
+          );
+        },
+    
+        // Alternate "entry point". Allows parsing a standalone expression.
+        semgrep_expression: $ => seq('__SEMGREP_EXPRESSION', $._expression),
+
+        // Metavariables
+        simple_identifier: ($, previous) => {
+            return choice(
+                previous,
+                /\$[a-zA-Z_][a-zA-Z_0-9]*/
+            )
+        },
+        
+        // Statement ellipsis: '...' not followed by ';'
+        _expression: ($, previous) => {
+            return choice(
+                previous,
+                $.ellipsis,  // statement ellipsis
+            );
+        },
+
+        ellipsis: $ => '...',
     }
 });
