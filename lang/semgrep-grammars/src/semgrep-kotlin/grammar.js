be91fe1c243b258a0b85b3a55c97f751a56f0a66
Martin Jambon <martin@returntocorp.com>
2021-02-05 17:38:35 -0800

    Run 'pre-commit run -a' to fix indentation and such

--- lang/semgrep-grammars/src/semgrep-kotlin/grammar.js
+++ lang/semgrep-grammars/src/semgrep-kotlin/grammar.js
@@ -19,7 +19,7 @@ module.exports = grammar(standard_grammar, {
             $.semgrep_expression
           );
         },
-    
+
         // Alternate "entry point". Allows parsing a standalone expression.
         semgrep_expression: $ => seq('__SEMGREP_EXPRESSION', $._expression),
 
@@ -30,7 +30,7 @@ module.exports = grammar(standard_grammar, {
                 /\$[a-zA-Z_][a-zA-Z_0-9]*/
             )
         },
-        
+
         // Statement ellipsis: '...' not followed by ';'
         _expression: ($, previous) => {
             return choice(
