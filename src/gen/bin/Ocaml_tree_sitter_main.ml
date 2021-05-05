a9c1348877b087ea0cafe3d2b9fad298f2df2539
José Eduardo <josee.edumont@gmail.com>
2021-05-05 20:38:44 -0300

    initial effort to change to multiple commands
    remove safe_run from general scope

--- src/gen/bin/Ocaml_tree_sitter_main.ml
+++ src/gen/bin/Ocaml_tree_sitter_main.ml
@@ -2,23 +2,18 @@
    Application's entrypoint.
 *)
 
-open Printf
 open Cmdliner
 open Tree_sitter_gen
 
-type config = {
+type parse_conf = {
   lang : string;
   grammar : string;
   out_dir : string option;
 }
 
-let codegen config =
-  let tree_sitter_grammar =
-    Atdgen_runtime.Util.Json.from_file Tree_sitter_j.read_grammar
-      config.grammar
-  in
-  let grammar = CST_grammar_conv.of_tree_sitter tree_sitter_grammar in
-  Codegen.ocaml ?out_dir:config.out_dir ~lang:config.lang grammar
+
+type cmd_conf =
+  | Parse of parse_conf
 
 let lang_term =
   let info =
@@ -31,8 +26,7 @@ let lang_term =
   Arg.required (Arg.pos 0 Arg.(some string) None info)
 
 let grammar_term =
-  let info =
-    Arg.info []
+  let info = Arg.info []
       ~docv:"GRAMMAR_JSON"
       ~doc:"$(docv) is a file containing a tree-sitter grammar in json format.
             Its name is commonly 'grammar.json'. Try to not confuse it with
@@ -49,71 +43,49 @@ let out_dir_term =
   in
   Arg.value (Arg.opt Arg.(some string) None info)
 
-let cmdline_term =
-  let combine lang grammar out_dir =
-    { lang; grammar; out_dir }
-  in
-  Term.(const combine
-        $ lang_term
-        $ grammar_term
-        $ out_dir_term
-       )
 
-let doc =
-  "derive ocaml code to interpret tree-sitter parsing output"
+let parse_cmd = 
+    
+  let codegen lang grammar out_dir =
+    let tree_sitter_grammar =
+      Atdgen_runtime.Util.Json.from_file Tree_sitter_j.read_grammar
+        grammar
+    in
+    let grammar = CST_grammar_conv.of_tree_sitter tree_sitter_grammar in
+    Codegen.ocaml ?out_dir ~lang grammar in
 
+  let cmdline_term =
+    Term.(
+          const codegen
+          $ lang_term
+          $ grammar_term
+          $ out_dir_term
+        ) in
+    
 
-let version = "0.0.0"
-
-let man = [
-  `S Manpage.s_description;
-  `P "ocaml-tree-sitter takes a tree-sitter grammar and generates OCaml
+  let doc = "derive ocaml code to interpret tree-sitter parsing output" in
+  let man = [
+    `S Manpage.s_description;
+    `P "ocaml-tree-sitter takes a tree-sitter grammar and generates OCaml
       for reading tree-sitter parsing output. The input grammar is in
       json format, commonly under the file name 'grammar.json'.
       tree-sitter itself is used separately to generate the actual C parser
       from the json grammar. The generated OCaml code is meant to be used
       to process the output of such a parser.";
-  `S Manpage.s_bugs;
-  `P "Check out bug reports at
+      `S Manpage.s_bugs;
+      `P "Check out bug reports at
       https://github.com/returntocorp/ocaml-tree-sitter/issues.";
-]
+    ] in
+  let version = "0.0.0" in
+  let info = Term.info ~version ~doc ~man "ocaml-tree-sitter" in
 
-let parse_command_line () =
-  let info =
-    Term.info
-      ~version
-      ~doc
-      ~man
-      "ocaml-tree-sitter"
-  in
-  match Term.eval (cmdline_term, info) with
-  | `Error _ -> exit 1
-  | `Version | `Help -> exit 0
-  | `Ok config -> config
+  (cmdline_term, info)
 
-let safe_run config =
-  try codegen config
-  with
-  | Failure msg ->
-      eprintf "\
-Error: %s
-Try --help.
-%!" msg;
-      exit 1
-  | e ->
-      let trace = Printexc.get_backtrace () in
-      eprintf "\
-Error: exception %s
-%s
-Try --help.
-%!"
-        (Printexc.to_string e)
-        trace;
-      exit 1
 
-let main () =
-  Printexc.record_backtrace true;
-  let config = parse_command_line () in
-  safe_run config
+let subcommands = []
 
-let () = main ()
+
+let () = Term.(exit @@ eval_choice parse_cmd subcommands)
+
+
+
