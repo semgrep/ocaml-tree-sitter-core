92e954056c616a1a94cf84a54486776644397b98
Martin Jambon <martin@r2c.dev>
2021-05-21 13:26:57 -0700

    Add maintenance note

--- lang/semgrep-grammars/src/semgrep-javascript/grammar.js
+++ lang/semgrep-grammars/src/semgrep-javascript/grammar.js
@@ -5,6 +5,17 @@
 
   Semgrep metavariables are already valid javascript
   identifiers so we do nothing for them.
+
+  Maintenance:
+
+  - Most tests are not in the tree-sitter format, but are under
+    /lang/javascript/test. To test the grammar, use:
+
+      cd lang
+      ./test-lang javascript
+
+  - Some of the rules aren't really extended but were copy-pasted then edited.
+    They may have to be updated more frequently. Tests should catch problems.
 */
 
 const javascript_grammar = require('tree-sitter-javascript/grammar');
