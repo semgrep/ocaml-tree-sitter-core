f4284d1acbefb62f40f86def010c051d7032383b
Martin Jambon <martin@r2c.dev>
2021-05-24 12:34:58 -0700

    Remove typescript-specific parts from failing test case

--- lang/javascript/test/ok/pfff/parsing/pattern_default.js
+++ lang/javascript/test/ok/pfff/parsing/pattern_default.js
@@ -1,17 +1,9 @@
 let { a, b = 2 } = foo;
 
-
-function   prettierError(err) {
-  let { details: _details = '', origin } = err;
+function prettierError(err) {
+  let { details = '', origin } = err;
 }
 
-
 const { object = {}, property = {} } = name;
-
-// typescript only
-//
-// const {
-//   reserved: isReservedNodeType = false,
-// } = nodeAttrs;
 
 let [/*match*/, theirName, myName = theirName] = tokens;
