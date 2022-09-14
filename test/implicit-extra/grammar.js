0a676968ea9d8bc7c65e0edbfb2c07973899100e
Martin Jambon <martin@r2c.dev>
2022-09-14 16:02:13 -0700

    Test and fix the extraction of extras, and in particular of extras embedded within other extras.

--- test/implicit-extra/grammar.js
+++ test/implicit-extra/grammar.js
@@ -7,8 +7,7 @@ module.exports = grammar({
   extras: ($) => [
     $.comma,
     $.period,
-    "\n",  // check that literals are tolerated
-    /a*/,  // check that patterns are tolerated
+    "\n",
   ],
 
   rules: {
@@ -30,8 +29,5 @@ module.exports = grammar({
     // Check that name collision avoidance works
     comma_explicit: ($) => 'unused',
     dummy_alias0: ($) => 'unused',
-
-    // This should get another type name than 'extra'
-    extra: ($) => 'unused',
   }
 });
