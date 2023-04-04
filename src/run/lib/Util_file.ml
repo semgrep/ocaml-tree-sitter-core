b4aa1a4ffdeb495ec0568d4b7e9d87bf1a1b2563
Tom Petr <tom@r2c.dev>
2023-04-03 20:57:43 -0400

    forgot to enable pre-commit

--- src/run/lib/Util_file.ml
+++ src/run/lib/Util_file.ml
@@ -6,7 +6,7 @@
  * so some functions can change their implementation and rely
  * less on non-portable API like Unix which does not work well under
  * node or in the browser.
- *)
+*)
 let jsoo = ref false
 
 (*
@@ -32,22 +32,22 @@ let jsoo = ref false
 
 let read_file path =
   if !jsoo then (let ic = open_in_bin path in
-  let s = really_input_string ic (in_channel_length ic) in
-  close_in ic;
-  s) else
-  let buf_len = 4096 in
-  let extbuf = Buffer.create 4096 in
-  let buf = Bytes.create buf_len in
-  let rec loop fd =
-    match Unix.read fd buf 0 buf_len with
-    | 0 -> Buffer.contents extbuf
-    | num_bytes ->
-        assert (num_bytes > 0);
-        assert (num_bytes <= buf_len);
-        Buffer.add_subbytes extbuf buf 0 num_bytes;
-        loop fd
-  in
-  let fd = Unix.openfile path [Unix.O_RDONLY] 0 in
-  Fun.protect
-    ~finally:(fun () -> Unix.close fd)
-    (fun () -> loop fd)
+                 let s = really_input_string ic (in_channel_length ic) in
+                 close_in ic;
+                 s) else
+    let buf_len = 4096 in
+    let extbuf = Buffer.create 4096 in
+    let buf = Bytes.create buf_len in
+    let rec loop fd =
+      match Unix.read fd buf 0 buf_len with
+      | 0 -> Buffer.contents extbuf
+      | num_bytes ->
+          assert (num_bytes > 0);
+          assert (num_bytes <= buf_len);
+          Buffer.add_subbytes extbuf buf 0 num_bytes;
+          loop fd
+    in
+    let fd = Unix.openfile path [Unix.O_RDONLY] 0 in
+    Fun.protect
+      ~finally:(fun () -> Unix.close fd)
+      (fun () -> loop fd)
