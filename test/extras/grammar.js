31b0c2a268c64c4884d5b99af7457ea4af3d3ac3
Martin Jambon <martin@mjambon.com>
2024-09-09 21:02:16 -0700

    Add a naming test

--- test/extras/grammar.js
+++ test/extras/grammar.js
@@ -8,10 +8,14 @@ module.exports = grammar({
     letter: $ => /[a-zA-Z]/,
     comment: $ => /#.*/,
     complex_extra: $ => seq('(', repeat(choice($.number, $.letter)), ')'),
+    extra: $ => 'extra',    // a rule named 'extra' (!)
+    extras: $ => 'extras',  // a rule named 'extras' (!)
   },
   extras: $ => [
     $.comment,       // appears in the CST
     $.complex_extra, // same
+    $.extra,
+    $.extras,
     "IGNOREME",  // doesn't appear in the CST because it doesn't have a name
     /\s|\\\n/    // same
   ]
