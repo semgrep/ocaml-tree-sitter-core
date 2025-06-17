3baf8688bb39730a900529a7225a742c67f6a90d
brandonspark <wu.brandonj@gmail.com>
2025-06-17 14:23:20 -0700

    remove jsoo

--- src/run/lib/Util_file.mli
+++ src/run/lib/Util_file.mli
@@ -2,9 +2,6 @@
    Generic file utilities not provided by OCaml.
 *)
 
-(* you should set this flag when you run code compiled by js_of_ocaml *)
-val jsoo : bool ref
-
 (* Read the contents of file.
 
    This implementation works even with Linux files like /dev/fd/63
