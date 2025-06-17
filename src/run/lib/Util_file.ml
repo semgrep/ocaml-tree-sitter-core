9e4f06a15ca643f0939de17d082d2e022ad13eae
brandonspark <wu.brandonj@gmail.com>
2025-06-17 14:45:49 -0700

    precommit

--- src/run/lib/Util_file.ml
+++ src/run/lib/Util_file.ml
@@ -24,19 +24,19 @@
 *)
 
 let read_file path =
-    let buf_len = 4096 in
-    let extbuf = Buffer.create 4096 in
-    let buf = Bytes.create buf_len in
-    let rec loop fd =
-      match Unix.read fd buf 0 buf_len with
-      | 0 -> Buffer.contents extbuf
-      | num_bytes ->
-          assert (num_bytes > 0);
-          assert (num_bytes <= buf_len);
-          Buffer.add_subbytes extbuf buf 0 num_bytes;
-          loop fd
-    in
-    let fd = Unix.openfile path [Unix.O_RDONLY] 0 in
-    Fun.protect
-      ~finally:(fun () -> Unix.close fd)
-      (fun () -> loop fd)
+  let buf_len = 4096 in
+  let extbuf = Buffer.create 4096 in
+  let buf = Bytes.create buf_len in
+  let rec loop fd =
+    match Unix.read fd buf 0 buf_len with
+    | 0 -> Buffer.contents extbuf
+    | num_bytes ->
+        assert (num_bytes > 0);
+        assert (num_bytes <= buf_len);
+        Buffer.add_subbytes extbuf buf 0 num_bytes;
+        loop fd
+  in
+  let fd = Unix.openfile path [Unix.O_RDONLY] 0 in
+  Fun.protect
+    ~finally:(fun () -> Unix.close fd)
+    (fun () -> loop fd)
